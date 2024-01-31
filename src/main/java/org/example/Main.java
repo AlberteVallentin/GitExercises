1431f8d5763f5837fe16f501e17c427c66fca23f
Alfredo Fernandez <aalagt@outlook.dk>
2024-01-31 19:50:09 +0100

    For loop to 100!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a=101;
+
+        for (int i =1; i<a; i++ ){
+            System.out.println(i);
+        }
     }
 }
