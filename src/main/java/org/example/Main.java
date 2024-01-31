52eff60a139fd4d937e046ba77429866e02da337
KamiStolt <k.stoltenberg@hotmail.com>
2024-01-31 20:09:10 +0100

    FizzBuzz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,18 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        for(int i = 1; i <=100; i++)
+        {
+            if (i % 3== 0 && i % 5==0)
+            {
+                System.out.println("FIzzBuzz");
+            } else if(i %3 == 0)
+            {
+                System.out.println("Buzz");
+            } else
+            {
+                System.out.println(i);
+            }
+        }
     }
 }
